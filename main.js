b3c3994ecb4a474647a7ca1f205aee37bb7bde23
dennituzwalpro-sudo <dennituzwalpro@gmail.com>
2025-10-16 23:07:25 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-const SUPABASE_URL = "YOUR_PROJECT_URL";
-const SUPABASE_KEY = "YOUR_ANON_KEY";
+const SUPABASE_URL = "https://nylutsjvyiqerbrsztnn.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im55bHV0c2p2eWlxZXJicnN6dG5uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA1OTEwODcsImV4cCI6MjA3NjE2NzA4N30.8zeOzwpSs_jhQxHQcFBXd6zEtVW4A_hXyviDFAtdrWE";
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
