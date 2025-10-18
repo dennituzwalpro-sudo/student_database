f5bf40cd0a3f61d42d237ad9e080dde477dd0511
dennituzwalpro-sudo <dennituzwalpro@gmail.com>
2025-10-18 18:02:02 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey);
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_KEY
+);
+
 
 
 import {
