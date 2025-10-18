04db5c06b0b231b8d9d1844f9fe33b5cc84e0eeb
dennituzwalpro-sudo <dennituzwalpro@gmail.com>
2025-10-18 18:12:42 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
   process.env.NEXT_PUBLIC_SUPABASE_KEY
 );
+console.log("Supabase URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
+console.log("Supabase Key:", process.env.NEXT_PUBLIC_SUPABASE_KEY);
 
 
 
